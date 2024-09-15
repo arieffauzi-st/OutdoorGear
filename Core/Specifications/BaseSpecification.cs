57a4d8317dd64605951f8be8539c2f61df185f56
arieffauzi-st <arieffauzi_st@gmail.com>
2024-09-16 02:42:49 +0700

    feat: Add sorting, filtering, searching, and pagination functionality

--- Core/Specifications/BaseSpecification.cs
+++ Core/Specifications/BaseSpecification.cs
@@ -1,21 +1,35 @@
-
-using System.Linq.Expressions;
+﻿using System.Linq.Expressions;
 using Core.Interfaces;
 
-namespace Core.Specifications
+namespace Core.Specifications;
+
+public class BaseSpecification<T>(Expression<Func<T, bool>>? criteria) : ISpecification<T>
 {
-    public class BaseSpecification<T>(Expression<Func<T, bool>>? criteria) :
-    ISpecification<T>
+    protected BaseSpecification() : this(null) { }
+
+    public Expression<Func<T, bool>>? Criteria => criteria;
+
+    public Expression<Func<T, object>>? OrderBy { get; private set; }
+
+    public Expression<Func<T, object>>? OrderByDescending { get; private set; }
+
+    public bool IsDistinct { get; private set; }
+
+    public int Take { get; private set; }
+
+    public int Skip { get; private set; }
+
+    public bool IsPagingEnabled { get; private set; }
+
+    public IQueryable<T> ApplyCriteria(IQueryable<T> query)
     {
-        protected BaseSpecification() : this(null) { }
+        if (Criteria != null)
+        {
+            query = query.Where(Criteria);
+        }
 
-        public Expression<Func<T, bool>>? Criteria => criteria;
-
-        public Expression<Func<T, object>>? OrderBy { get; private set; }
-
-        public Expression<Func<T, object>>? OrderByDescending { get; private set; }
-
-        public bool IsDistinct { get; private set; }
+        return query;
+    }
 
     protected void AddOrderBy(Expression<Func<T, object>> orderByExpression)
     {
@@ -31,19 +45,23 @@ namespace Core.Specifications
     {
         IsDistinct = true;
     }
-        
-    }
 
-    public class BaseSpecification<T, TResult>(Expression<Func<T, bool>> criteria) :
-    BaseSpecification<T>(criteria), ISpecification<T, TResult>
+    protected void ApplyPaging(int skip, int take)
     {
-    protected BaseSpecification() : this(null!) { }
-    public Expression<Func<T, TResult>>? Select { get; private set; }
-
-    protected void AddSelect(Expression<Func<T, TResult>> selectExpression)
-    {
-        Select = selectExpression;
+        Skip = skip;
+        Take = take;
+        IsPagingEnabled = true;
     }
-    }
-
 }
+
+    public class BaseSpecification<T, TResult>(Expression<Func<T, bool>> criteria)
+        : BaseSpecification<T>(criteria), ISpecification<T, TResult>
+    {
+        protected BaseSpecification() : this(null!) { }
+        public Expression<Func<T, TResult>>? Select { get; private set; }
+
+        protected void AddSelect(Expression<Func<T, TResult>> selectExpression)
+        {
+            Select = selectExpression;
+        }
+    }
